c0e496276a7905837c3b5bb5b8153bf5d08c90ab
SYTC307u8365 <SYTC307u8365@ad.pcc.edu>
2017-10-24 13:18:20 -0700

    Adding unit tests to Database and User

---
+++ src/UserTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by SYTC307u8365 on 10/24/2017.
+ */
+class UserTest {
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void getUserID() {
+        User u = new User(-1, "marc.goodman@pcc.edu", "test password", "Admin");
+
+        assertEquals(u.getUserID(), -1);
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getRole() {
+    }
+
+}
